f6b09907153696536bd4a28b5b994ccc51b2dd8c
DaveatCor <condaveat@gmail.com>
2019-03-19 15:32:10 +0700

    offline data

--- lib/src/resources/news_db_provider.dart
+++ lib/src/resources/news_db_provider.dart
@@ -4,3 +4,57 @@ import 'dart:io';
 import 'package:path/path.dart';
 import 'dart:async';
 import '../models/item_model.dart';
+
+class NewDbProvider {
+  Database db;
+  
+  init() async {
+    Directory documentDirectory = await getApplicationDocumentsDirectory();
+
+    final path =join(documentDirectory.path, "items.db");
+
+    db = await openDatabase(
+      path,
+      version: 1,
+      onCreate: (Database newDb, int version) {
+        newDb.execute("""
+          Create TABLE Items
+          (
+            id INTEGER PRIMARY KEY,
+            delelted INTEGER,
+            type TEXT,
+            by TEXT, 
+            time INTEGER,
+            text TEXT,
+            dead INTEGER,
+            parent INTETER,
+            kids BLOB,
+            url TEXT,
+            score TEXT,
+            title TEXT,
+            parts TEXT,
+            descendants INTEGER
+          )
+        """);
+      }
+    );
+  }
+
+  Future<ItemModel> fetchItem(int id) async {
+    final maps = await db.query(
+      "Items",
+      columns: null,
+      where: "id = ?",
+      whereArgs: [id]
+    );
+
+    if (maps.length > 0 ) {
+      return ItemModel.fromDB(maps.first);
+    }
+    return null;
+  }
+
+  Future<int> addItem(ItemModel items) {
+    return db.insert("Items", items.toMap());
+  }
+}
